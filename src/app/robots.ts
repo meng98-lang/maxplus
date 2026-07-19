4aa8b30ffe1ca37ea0d0e21f719ce8f833bdb6fd
meng98-lang <mengzhongqiu98@gmail.com>
2026-07-19 20:15:47 +0800

    Update robots.tsUpdate robots.ts: replace base URL with chuanjing-max.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,23 +1,15 @@
 import { MetadataRoute } from 'next';
-
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.COZE_PROJECT_DOMAIN_DEFAULT 
-    ? `https://${process.env.COZE_PROJECT_DOMAIN_DEFAULT}` 
-    : 'https://maxplus.com';
-
+  const baseUrl = process.env.COZE_PROJECT_DOMAIN_DEFAULT ? `https://${process.env.COZE_PROJECT_DOMAIN_DEFAULT}` : 'https://chuanjing-max.com';
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/admin', '/api/'],
-      },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/admin', '/api/'],
-      },
-    ],
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/admin', '/api/'] }, { userAgent: 'Googlebot', allow: '/', disallow: ['/admin', '/api/'] }],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}import { MetadataRoute } from 'next';
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.COZE_PROJECT_DOMAIN_DEFAULT ? `https://${process.env.COZE_PROJECT_DOMAIN_DEFAULT}` : 'https://chuanjing-max.com';
+  return {
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/admin', '/api/'] }, { userAgent: 'Googlebot', allow: '/', disallow: ['/admin', '/api/'] }],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
